f74e27d20ec3166e523678a6d0b968d908f4f8e2
pawel <wp39484@zut.edu.pl>
2019-04-16 22:46:44 +0200

    tworzenie listy

--- app/src/main/java/com/example/lab4/MainActivity.java
+++ app/src/main/java/com/example/lab4/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.lab4;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +16,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //todo
     }
+
+    private ArrayList<String> target;
+    private ArrayAdapter adapter;
+
+
 }
+
